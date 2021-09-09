fab51db0deb3c4d4d63d68ce53251326abce5ae6
Niels Rogge <niels.rogge1@gmail.com>
2021-09-09 11:23:29 +0200

    Fix bug in print statement

--- perceiver/perceiver.py
+++ perceiver/perceiver.py
@@ -67,7 +67,7 @@ def attend(q, k, v, dropout_prob=0.0, attention_mask=None):
     attention = jnp.where(attention_mask[:, None, :, :], attention,
                           -large_k)
 
-  print("Attention scores after applying mask:", attention[0,:3,:3:3])
+  print("Attention scores after applying mask:", attention[0,:3,:3,:3])
   
   normalized = jax.nn.softmax(attention)
   if dropout_prob > 0:
